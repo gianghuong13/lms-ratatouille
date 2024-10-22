404e8527f02d8e59c357f1c40cd8428343e01b6b
Than Viet Anh <vietanh.vadc.2004@gmail.com>
2024-10-22 12:38:34 +0700

    Hiển thị Màn hình đăng nhập

---
+++ Ratatouille/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Login from './Login.jsx';
+import './index.css';
+
+const App = () => {
+  return <Login />;
+};
+
+export default App;
---
+++ Ratatouille/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: { "fuschia-100": "var(--fuschia-100)", "fuschia-60": "var(--fuschia-60)", "fuschia-80": "var(--fuschia-80)", "iris-100": "var(--iris-100)", "iris-60": "var(--iris-60)", "iris-80": "var(--iris-80)", }, fontFamily: { body: "var(--body-font-family)", "header-1": "var(--header-1-font-family)", "header-2": "var(--header-2-font-family)", }
+    },
+  },
+  plugins: [],
+}
