263e9e85c54d77754e5d3ccab14c7ce9ec74d8f4
ntqqtn <nguyenthuyquynh27102004@gmail.com>
2024-11-13 14:29:08 +0700

    Add .gitignore server

--- Server-Ratatouille/routes/api.js
+++ Server-Ratatouille/routes/api.js
@@ -2,9 +2,12 @@ import express from "express";
 var router = express.Router();
 import authRouter from "./auth/auth.js";
 import userManageRouter from "./user-manage/user-manage.js";
+import notiManageRouter from "./noti-manage/noti-manage.js";
+
 
 router.use(authRouter);
 router.use(userManageRouter);
+router.use(notiManageRouter);
 
 
 export default router;
