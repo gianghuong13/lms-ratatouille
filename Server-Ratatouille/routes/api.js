64186f1344d5e93e2cebd21dc7666170a9872407
gianghuong13 <nhg130904@gmail.com>
2024-11-13 13:25:24 +0700

    edit addcourseform, add course controller and api

--- Server-Ratatouille/routes/api.js
+++ Server-Ratatouille/routes/api.js
@@ -2,9 +2,10 @@ import express from "express";
 var router = express.Router();
 import authRouter from "./auth/auth.js";
 import userManageRouter from "./user-manage/user-manage.js";
+import courseManageRouter from "./course-manage/course-manage.js";
 
 router.use(authRouter);
 router.use(userManageRouter);
-
+router.use(courseManageRouter);
 
 export default router;
