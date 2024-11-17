3d100c798ca04620ded41634ec93a8619fc7eef6
ntqqtn <nguyenthuyquynh27102004@gmail.com>
2024-11-18 02:30:19 +0700

    update notification

--- Client-Ratatouille/src/features/admin/NotiLists.jsx
+++ Client-Ratatouille/src/features/admin/NotiLists.jsx
@@ -16,16 +16,17 @@ export default function NotiLists(){
     return(
         <div>
             {notifications.length > 0 ? (
-                <ul>
+                <ul >
                     {
-                        notifications.map((noti)=>
-                            <li key={noti.notification_id} >
-                                <Link className="flex flex-row mx-4 p-2 border-b-2 border-[#D6CDCD]">
+                        notifications.map((noti, index)=>
+                            <li key={noti.notification_id}>
+                                <Link className="flex flex-row mx-1 md:mx-3 xl:mx-10 py-1 px-2 border-t-[1px] border-[#D6CDCD] hover: hover:bg-[#f1f5fd]"
+                                    to={"/admin/notifications/update-notification/"+noti.notification_id}>
                                 <div className="mr-2 my-1"><img className="object-fill w-full min-w-[30px] max-w-[40px] h-auto aspect-square" src={logo} alt="admin-logo" /></div>
                                 <div>
-                                    <h6 className="font-bold font-sans">{noti.title}</h6>
-                                    <p>{noti.content}</p>
-                                    <p className="text-sm italic">{noti.created_date}</p>
+                                    <h6 className="font-bold font-sans m-0">{noti.title}</h6>
+                                    <p className="m-0">{noti.content}</p>
+                                    <p className="text-sm italic m-0">{noti.created_date}</p>
                                 </div>
                                 </Link>
                             </li>
@@ -36,3 +37,7 @@ export default function NotiLists(){
         </div>
     );
 }
+
+// className={`hover:bg-[#f1f5fd] ${
+//     index % 2 === 1 ? "bg-gray-50" : "" // Áp dụng màu xám cho các phần tử chẵn (bắt đầu từ 0)
+//   }`
