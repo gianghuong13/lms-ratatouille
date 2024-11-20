a3dc65036405b5fc40aef0b878ee8b8e9aebeae4
Than Viet Anh <vietanh.vadc.2004@gmail.com>
2024-11-20 21:36:49 +0700

    navabar student

--- Client-Ratatouille/src/App.jsx
+++ Client-Ratatouille/src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage.jsx';
 import MePage from './pages/Admin/MePage.jsx';
 import TeacherPage from './pages/Teacher/MePage.jsx';
-import StudentPage from './pages/Student/MePage.jsx';
 import ProtectedRoutes from './middlewares/ProtectedRoute.jsx';
 import UnauthorizedRoutes from './middlewares/UnauthorizedRoute.jsx';
 import DashboardPage from './pages/Admin/Dashboard.jsx';
@@ -16,14 +15,16 @@ import CreateNotification from './pages/Admin/NotiManage/CreateNotification.jsx'
 import UpdateNotification from './pages/Admin/NotiManage/UpdateNotification.jsx';
 
 import 'global';
+import Dashboard from './pages/Admin/Dashboard.jsx';
+import DashboardHome from './pages/Student/StudentPage.jsx';
 
 const App = () => {
   return (
     <Routes>
       <Route element={<ProtectedRoutes />}>
         <Route path="/admin" >
-          <Route index element={<DashboardPage />} />
-          <Route path="" element={<DashboardPage />} />
+          <Route index element={<Dashboard />} />
+          <Route path="" element={<Dashboard />} />
           <Route path="me" element={<MePage />} />
           <Route path='courses'element={<CourseManagePage />} />
           <Route path='courses/add' element={<AddCoursePage />} />
@@ -33,7 +34,10 @@ const App = () => {
           <Route path="notifications/update-notification/:id" element={<UpdateNotification />} />
         </Route>
         <Route path="/teacher" element={<TeacherPage />} />
-        <Route path="/student" element={<StudentPage />} />
+        <Route path="/student">
+          <Route index element={<DashboardHome />} />
+          
+        </Route>
       </Route>
       <Route element={<UnauthorizedRoutes />}>
         <Route path="/" element={<Navigate to="login" replace />} />
