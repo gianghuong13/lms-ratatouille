47d2bfb0450a5cc6c355609e669fb18a78564762
chip1911 <bts13061911@gmail.com>
2024-12-05 15:39:32 +0700

    fix conflict

---
+++ Client-Ratatouille/src/pages/Users/Course/CoursePeople.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../../../components/Layout'
+const CoursePeople = () => {
+  return (
+    <Layout>
+      <div>CoursePeople</div>
+    </Layout>
+  )
+}
+
+export default CoursePeople
