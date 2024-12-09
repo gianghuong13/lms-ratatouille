a43cf0c3346276e642af9119a691818f3bd2a762
Than Viet Anh <vietanh.vadc.2004@gmail.com>
2024-12-09 22:29:03 +0700

    responsive

--- Client-Ratatouille/src/pages/Users/Course/FileDetail.jsx
+++ Client-Ratatouille/src/pages/Users/Course/FileDetail.jsx
@@ -41,41 +41,33 @@ const FileDetails = () => {
 
   return (
     <Layout>
-      {/* Điều chỉnh vị trí bằng Tailwind CSS */}
-      <div className="-mt-7">
-        {file && (
-          <div className="mt-4">
-            {/* Tiêu đề */}
-            <p className="text-black font-outfit text-2xl font-normal">
-              {file.fileName}
-            </p>
-
-            {/* Link Tải về */}
-            <a
-              href={file.signedUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 font-outfit text-lg font-normal underline"
-            >
-              Download File {file.fileName}
-            </a>
-
-            {/* Hiển thị Tài liệu */}
-            <div
-              className="w-[1000px] h-[550px] overflow-y-auto border border-gray-300 rounded-lg p-2"
-            >
-              <DocViewer
-                documents={[{ uri: file.signedUrl, fileType: file.fileType }]}
-                pluginRenderers={DocViewerRenderers}
-                style={{
-                  width: "100%",
-                  height: "100%",
-                }}
-              />
-            </div>
-          </div>
-        )}
+      <div className="p-4 sm:p-6">
+  {file && (
+    <div className="mt-4">
+      <p className="text-black font-outfit text-xl sm:text-2xl font-normal">
+        {file.fileName}
+      </p>
+      <a
+        href={file.signedUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-500 font-outfit text-base sm:text-lg font-normal underline"
+      >
+        Download File {file.fileName}
+      </a>
+      <div className="w-full h-[300px] sm:h-[550px] overflow-y-auto border border-gray-300 rounded-lg p-2 mt-4">
+        <DocViewer
+          documents={[{ uri: file.signedUrl, fileType: file.fileType }]}
+          pluginRenderers={DocViewerRenderers}
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        />
       </div>
+    </div>
+  )}
+</div>
     </Layout>
   );
 };
